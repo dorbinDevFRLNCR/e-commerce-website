5ff6bce8cab3543856ff007029055444ca3636da
dorbin.dev <dorbin.dev008@gmail.com>
2026-02-08 00:01:19 +0100

    Added Toaster

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Gallery from './components/gallery/gallery'
 import Inspirations from './components/inspirations/inspirations'
 import Research from './components/research/research'
 import Footer from './components/footer/footer'
+import { Toaster } from 'react-hot-toast'
 
 function App() {
   return (
@@ -19,6 +20,31 @@ function App() {
         <Gallery />
       </main>
       <Footer />
+      <Toaster
+        position="top-right"
+        reverseOrder={false}
+        toastOptions={{
+          duration: 3000,
+          style: {
+            background: '#363636',
+            color: '#fff',
+          },
+          success: {
+            duration: 3000,
+            iconTheme: {
+              primary: '#10b981',
+              secondary: '#fff',
+            },
+          },
+          error: {
+            duration: 4000,
+            iconTheme: {
+              primary: '#ef4444',
+              secondary: '#fff',
+            },
+          },
+        }}
+      />
     </>
   )
 }
